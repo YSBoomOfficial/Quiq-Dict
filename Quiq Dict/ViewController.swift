9bf466dfef6a117ad71edc31dcd7ae52476a9f25
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-08-02 02:10:12 +0100

    Initial Commit

---
+++ Quiq Dict/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiq Dict
+//
+//  Created by Yash Shah on 02/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
