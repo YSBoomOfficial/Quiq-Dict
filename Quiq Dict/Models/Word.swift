bd73230deb91af700d7dcc16874b359c25f9e8f9
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-08-02 03:01:00 +0100

    Add `Word` Model

---
+++ Quiq Dict/Models/Word.swift
@@ -0,0 +1,47 @@
+//
+//  Word.swift
+//  Quiq Dict
+//
+//  Created by Yash Shah on 02/08/2022.
+//
+
+import Foundation
+
+// MARK: Word
+struct Word: Codable {
+	let word: String
+	let phonetic: String?
+	let phonetics: [Phonetic]
+	let meanings: [Meaning]
+	let license: License
+	let sourceUrls: [String]
+
+	// MARK: Word.Phonetic
+	struct Phonetic: Codable {
+		let text: String?
+		let audio: String
+		let sourceUrl: String?
+		let license: License?
+	}
+
+	// MARK: Word.Meaning
+	struct Meaning: Codable {
+		let partOfSpeech: String
+		let definitions: [Definition]
+		let synonyms: [String]
+		let antonyms: [String]
+
+		// MARK: Word.Meaning.Definition
+		struct Definition: Codable {
+			let definition: String
+			let synonyms, antonyms: [String]
+			let example: String?
+		}
+	}
+
+	// MARK: License
+	struct License: Codable {
+		let name: String
+		let url: String
+	}
+}
