ae326303840b2a1b25f58dd8302b2261d86ea22e
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-10-16 00:45:00 +0100

    Move example for `Word` to separate file and move helper computed properties to extensions in the same file

---
+++ Quiq Dict/Models/Word+Example.swift
@@ -0,0 +1,111 @@
+//
+//  Word+Example.swift
+//  Quiq Dict
+//
+//  Created by Yash Shah on 15/10/2022.
+//
+
+import Foundation
+
+extension Word {
+	static let example = Word(
+		word: "hello",
+		phonetic: nil,
+		phonetics: [
+			.init(
+				text: nil,
+				audio: "https://api.dictionaryapi.dev/media/pronunciations/en/hello-au.mp3",
+				sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=75797336",
+				license: .init(
+					name: "BY-SA 4.0",
+					url: "https://creativecommons.org/licenses/by-sa/4.0"
+				)
+			),
+			.init(
+				text: "/həˈləʊ/",
+				audio: "https://api.dictionaryapi.dev/media/pronunciations/en/hello-uk.mp3",
+				sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=9021983",
+				license: .init(
+					name: "BY 3.0 US",
+					url: "https://creativecommons.org/licenses/by/3.0/us"
+				)
+			),
+			.init(
+				text: "/həˈloʊ/",
+				audio: "",
+				sourceUrl: nil,
+				license: nil
+			)
+		],
+		meanings: [
+			.init(
+				partOfSpeech: "noun",
+				definitions: [
+					.init(
+						definition: "\"Hello!\" or an equivalent greeting.",
+						synonyms: [],
+						antonyms: [],
+						example: nil
+					),
+				],
+				synonyms: ["greeting"],
+				antonyms: []
+			),
+			.init(
+				partOfSpeech: "verb",
+				definitions: [
+					.init(
+						definition: "To greet with \"hello\".",
+						synonyms: [],
+						antonyms: [],
+						example: nil
+					)
+				],
+				synonyms: [],
+				antonyms: []
+			),
+			.init(
+				partOfSpeech: "interjection",
+				definitions: [
+					.init(
+						definition: "A greeting (salutation) said when meeting someone or acknowledging someone’s arrival or presence.",
+						synonyms: [],
+						antonyms: [],
+						example: "Hello, everyone."
+					),
+					.init(
+						definition: "A greeting used when answering the telephone.",
+						synonyms: [],
+						antonyms: [],
+						example: "Hello? How may I help you?"
+					),
+					.init(
+						definition: "A call for response if it is not clear if anyone is present or listening, or if a telephone conversation may have been disconnected.",
+						synonyms: [],
+						antonyms: [],
+						example: "Hello? Is anyone there?"
+					),
+					.init(
+						definition: "Used sarcastically to imply that the person addressed or referred to has done something the speaker or writer considers to be foolish.",
+						synonyms: [],
+						antonyms: [],
+						example: "You just tried to start your car with your cell phone. Hello?"
+					),
+					.init(
+						definition: "An expression of puzzlement or discovery.",
+						synonyms: [],
+						antonyms: [],
+						example: "Hello! What’s going on here?"
+					)
+				],
+				synonyms: [],
+				antonyms: ["bye", "goodbye"]
+			)
+		],
+		license: .init(
+			name: "CC BY-SA 3.0",
+			url: "https://creativecommons.org/licenses/by-sa/3.0"
+		),
+		sourceUrls: ["https://en.wiktionary.org/wiki/hello"]
+	)
+}
--- Quiq Dict/Models/Word.swift
+++ Quiq Dict/Models/Word.swift
@@ -22,19 +22,6 @@ struct Word: Codable, Equatable {
 		let audio: String
 		let sourceUrl: String?
 		let license: License?
-
-		var audioAccentRegion: String? {
-			guard !audio.isEmpty else { return nil }
-			return audio.components(separatedBy: "/").last?.components(separatedBy: ".").first?.components(separatedBy: "-").last?.uppercased()
-		}
-
-		var displayText: String {
-			text ?? "N/a"
-		}
-
-		var noValues: Bool {
-			text == nil && audio.isEmpty && sourceUrl == nil && license == nil
-		}
 	}
 
 	// MARK: Word.Meaning
@@ -58,7 +45,10 @@ struct Word: Codable, Equatable {
 		let name: String
 		let url: String
 	}
+}
 
+// MARK: Word + Helpers
+extension Word {
 	var title: String {
 		word.capitalized
 	}
@@ -78,105 +68,26 @@ struct Word: Codable, Equatable {
 	}
 }
 
-extension Word {
-	static let example = Word(
-		word: "hello",
-		phonetic: nil,
-		phonetics: [
-			.init(
-				text: nil,
-				audio: "https://api.dictionaryapi.dev/media/pronunciations/en/hello-au.mp3",
-				sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=75797336",
-				license: .init(
-					name: "BY-SA 4.0",
-					url: "https://creativecommons.org/licenses/by-sa/4.0"
-				)
-			),
-			.init(
-				text: "/həˈləʊ/",
-				audio: "https://api.dictionaryapi.dev/media/pronunciations/en/hello-uk.mp3",
-				sourceUrl: "https://commons.wikimedia.org/w/index.php?curid=9021983",
-				license: .init(
-					name: "BY 3.0 US",
-					url: "https://creativecommons.org/licenses/by/3.0/us"
-				)
-			),
-			.init(
-				text: "/həˈloʊ/",
-				audio: "",
-				sourceUrl: nil,
-				license: nil
-			)
-		],
-		meanings: [
-			.init(
-				partOfSpeech: "noun",
-				definitions: [
-					.init(
-						definition: "\"Hello!\" or an equivalent greeting.",
-						synonyms: [],
-						antonyms: [],
-						example: nil
-					),
-				],
-				synonyms: ["greeting"],
-				antonyms: []
-			),
-			.init(
-				partOfSpeech: "verb",
-				definitions: [
-					.init(
-						definition: "To greet with \"hello\".",
-						synonyms: [],
-						antonyms: [],
-						example: nil
-					)
-				],
-				synonyms: [],
-				antonyms: []
-			),
-			.init(
-				partOfSpeech: "interjection",
-				definitions: [
-					.init(
-						definition: "A greeting (salutation) said when meeting someone or acknowledging someone’s arrival or presence.",
-						synonyms: [],
-						antonyms: [],
-						example: "Hello, everyone."
-					),
-					.init(
-						definition: "A greeting used when answering the telephone.",
-						synonyms: [],
-						antonyms: [],
-						example: "Hello? How may I help you?"
-					),
-					.init(
-						definition: "A call for response if it is not clear if anyone is present or listening, or if a telephone conversation may have been disconnected.",
-						synonyms: [],
-						antonyms: [],
-						example: "Hello? Is anyone there?"
-					),
-					.init(
-						definition: "Used sarcastically to imply that the person addressed or referred to has done something the speaker or writer considers to be foolish.",
-						synonyms: [],
-						antonyms: [],
-						example: "You just tried to start your car with your cell phone. Hello?"
-					),
-					.init(
-						definition: "An expression of puzzlement or discovery.",
-						synonyms: [],
-						antonyms: [],
-						example: "Hello! What’s going on here?"
-					)
-				],
-				synonyms: [],
-				antonyms: ["bye", "goodbye"]
-			)
-		],
-		license: .init(
-			name: "CC BY-SA 3.0",
-			url: "https://creativecommons.org/licenses/by-sa/3.0"
-		),
-		sourceUrls: ["https://en.wiktionary.org/wiki/hello"]
-	)
+// MARK: Word.Phonetic + Helpers
+extension Word.Phonetic {
+	var audioURL: String? {
+		audio.isEmpty ? nil : audio
+	}
+
+	var filename: String? {
+		audioURL?.components(separatedBy: "/").last?.replacingOccurrences(of: ".mp3", with: "")
+	}
+
+	var audioAccentRegion: String? {
+		guard let url = audioURL else { return nil }
+		return url.components(separatedBy: "/").last?.components(separatedBy: ".").first?.components(separatedBy: "-").last?.uppercased()
+	}
+
+	var displayText: String {
+		text ?? "N/a"
+	}
+
+	var noValues: Bool {
+		text == nil && audioAccentRegion == nil && sourceUrl == nil && license == nil
+	}
 }
